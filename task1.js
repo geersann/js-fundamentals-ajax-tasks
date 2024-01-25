8fd4dc0570821cbacca200eb6679df387c5d1b07
geersann <148795588+geersann@users.noreply.github.com>
2024-01-25 02:06:43 +0000

    change task1 - task2

--- task1.js
+++ task1.js
@@ -5,3 +5,25 @@
 // який створений у файлі index.html
 // Запустити програму за допомогою Live Server
 // Перевірити за допомогою команди npm tests/task1.test.js 
+
+
+const usersApi = 'https://jsonplaceholder.typicode.com/users';
+const list = document.querySelector(".usersList");
+
+fetch(usersApi)
+    .then(res => {
+        console.log("Response status:", res.status);
+        return res.json();
+    })
+    .then(data => {
+        if (data) {
+            const users = data;
+    
+            users.forEach(user => {
+                list.innerHTML += `<li>${user.name}</li>`;
+            });
+        } else {
+            console.error("Invalid users");
+        }
+    });
+
--- task2.js
+++ task2.js
@@ -5,3 +5,16 @@
 // відобразити у тезі #userCity
 // Запустити програму потрібно за допомогою Live Server
 // Перевірити правильність програми - команда node tests/task2.test.js
+
+document.getElementById('getUserButton').addEventListener('click', async () => {
+
+    const userName = document.getElementById('userNameInput').value.trim();
+
+    const url = `https://jsonplaceholder.typicode.com/users?name=${encodeURIComponent(userName)}`;
+    const response = await fetch(url);
+    const data = await response.json();
+
+    const userCity = data.length > 0 ? data[0].address.city : null;
+    document.getElementById('userCity').innerText = userCity ? `User's city: ${userCity}` : 'User not found';
+
+});
